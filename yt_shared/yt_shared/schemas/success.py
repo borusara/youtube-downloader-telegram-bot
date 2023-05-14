10f8e35e5eefda6604ca49640b9c1902252888c3
borusara <125817831+borusara@users.noreply.github.com>
2023-05-14 10:34:15 +0530

    Create success.py

---
+++ yt_shared/yt_shared/schemas/success.py
@@ -0,0 +1,22 @@
+import uuid
+from typing import Literal
+
+from pydantic import StrictInt, StrictStr
+
+from yt_shared.enums import RabbitPayloadType, TelegramChatType
+from yt_shared.schemas.base import BaseRabbitPayloadModel
+from yt_shared.schemas.media import DownMedia, InbMediaPayload
+
+
+class SuccessPayload(BaseRabbitPayloadModel):
+    """Payload with downloaded media context."""
+
+    type: Literal[RabbitPayloadType.SUCCESS] = RabbitPayloadType.SUCCESS
+    task_id: uuid.UUID
+    from_chat_id: StrictInt | None
+    from_chat_type: TelegramChatType | None
+    from_user_id: StrictInt | None
+    message_id: StrictInt | None
+    media: DownMedia
+    context: InbMediaPayload
+    yt_dlp_version: StrictStr | None
